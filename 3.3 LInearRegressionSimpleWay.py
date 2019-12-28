9739afc2e7fce1b20cd133c5408afb8148cabe34
LonnieQin <354060093@qq.com>
2019-12-28 22:48:57 +0800

    Add README.md

--- 3.3 LInearRegressionSimpleWay.py
+++ 3.3 LInearRegressionSimpleWay.py
@@ -1,27 +1,24 @@
-from mxnet import autograd, nd
-from base import lineareg, squared_loss, sgd
-from mxnet.gluon import data as gdata, nn
-from mxnet import init
-from mxnet.gluon import loss as gloss
-from mxnet import gluon
+from mxnet import autograd, nd, init, gluon
+from mxnet.gluon import data as gdata, nn, loss as gloss
 num_inputs = 2
 num_examples = 1000
+batch_size = 10
+num_epochs = 10
 true_w = nd.array([2, -3.4])
 true_b = nd.array([4.2])
 features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
 w = nd.random.normal(scale=1, shape=true_w.shape)
 b = nd.random.normal(scale=1, shape=true_b.shape)
-labels = lineareg(features, true_w, true_b)
+labels = nd.dot(features, true_w) + true_b
 labels += nd.random.normal(scale=0.1, shape=labels.shape)
-batch_size = 10
 dataset = gdata.ArrayDataset(features, labels)
 data_iter = gdata.DataLoader(dataset, batch_size, shuffle=True)
 net = nn.Sequential()
 net.add(nn.Dense(1))
 net.initialize(init.Normal(sigma=0.01))
 loss = gloss.L2Loss()
-trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
-num_epochs = 10
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.03})
+
 for epoch in range(1, num_epochs + 1):
     for X, y in data_iter:
         with autograd.record():
@@ -30,4 +27,6 @@ for epoch in range(1, num_epochs + 1):
         trainer.step(batch_size)
     l = loss(net(features), labels)
     print('Epoch:%d, loss: %f' % (epoch, l.mean().asnumpy()))
-    print("params:", net.collect_params())
+    dense = net[0]
+    print(true_w, dense.weight.data())
+    print(true_b, dense.bias.data())
---
+++ README.md
@@ -0,0 +1,2 @@
+This Project includes many demos about creating a deep learning project.
+1.
