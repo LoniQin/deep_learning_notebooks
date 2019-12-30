123f68c05c3f96a94c83bf1727f3bf67daf82566
LonnieQin <354060093@qq.com>
2019-12-30 18:00:34 +0800

    Train mnist with dropout method.

---
+++ fashion_mnist_v7.py
@@ -0,0 +1,29 @@
+from mxnet import nd, init, gluon
+from mxnet.gluon import loss as gloss, nn
+import utils
+batch_size = 256
+num_inputs = 784
+num_outputs = 10
+num_hiddens = 256
+num_epochs = 20
+dropout_prob1 = 0.2
+dropout_prob2 = 0.5
+learning_rate = 0.5
+train_iter, test_iter = utils.load_fashion_mnist(batch_size)
+net = nn.Sequential()
+net.add(nn.Dense(num_hiddens, activation='relu'))
+net.add(nn.Dropout(dropout_prob1))
+net.add(nn.Dense(num_hiddens, activation='relu'))
+net.add(nn.Dropout(dropout_prob2))
+net.add(nn.Dense(num_outputs))
+net.initialize(init.Normal(sigma=0.01))
+
+loss = gloss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': learning_rate})
+utils.train_mnist(net = net,
+                  train_iter=train_iter,
+                  test_iter = test_iter,
+                  loss = loss,
+                  num_epochs = num_epochs,
+                  batch_size = batch_size,
+                  trainer=trainer)
